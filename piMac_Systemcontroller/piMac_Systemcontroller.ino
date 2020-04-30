a445fd19f068f7829d14e7fa0ac01e57f0ad3db2
coconut147 <github@resueh.de>
2020-04-30 21:38:24 +0200

    Moved GPIO defines to headerfile

--- piMac_Systemcontroller/piMac_Systemcontroller.ino
+++ piMac_Systemcontroller/piMac_Systemcontroller.ino
@@ -33,16 +33,7 @@ PowerDown -> Running : LongPowerButton pressed
 */
 
 
-#define IN_TEMP 0
-#define IN_POWER_BTN 2
-#define IN_I2C_SDA 4
-#define IN_I2C_SCL 5
-
-#define OUT_DISP_BACKGROUND 3 
-#define OUT_FAN_12V 5
-#define OUT_STATUS_LED 6
-#define OUT_SERVO 9
-#define OUT_POWER_5V 12
+#include "gpio_config.h"
 
 
 
